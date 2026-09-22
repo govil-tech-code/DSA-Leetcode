c40d2e0bac02cf0d1ff7a14c57ccf35a2ace3768
UTTAM GOVIL <goviluttam360@gmail.com>
2026-09-22 18:29:09 +0530

    Time: 1 ms (99.05%) | Memory: 42.8 MB (32.11%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        int ans = 0;
+
+        while (a >= b) {
+            long temp = b;
+            long multiple = 1;
+            while (a >= temp + temp) {
+                temp = temp + temp;
+                multiple = multiple + multiple;
+            }
+            a = a - temp;
+            ans = ans + (int) multiple;
+        }
+        if ((dividend < 0) != (divisor < 0)) {
+            ans = -ans;
+        }
+        return ans;
+    }
+}
